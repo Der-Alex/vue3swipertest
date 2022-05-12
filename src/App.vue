ea2f2a0409c72498876494a2d8d70737f1b04682
Der Alex <alexander.thimm@googlemail.com>
2022-05-12 12:10:57 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <p>
+        <RouterLink to="/">Home</RouterLink>
+      </p>
+      <p>
+        <RouterLink to="/about">About</RouterLink>
+      </p>
+    </div>
+  </header>
+  <RouterView />
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <h1>Home</h1>
+
+    <swiper :slides-per-view="3" :auto-height="true" :centered-slides="true">
+      <swiper-slide>
+        <div class="blub">
+          <p>1</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="blub">
+          <p>2</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="blub">
+          <p>3</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="blub">
+          <p>4</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="blub">
+          <p>5</p>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="blub">
+          <p>6</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </main>
+</template>
+
+<script lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css/bundle';
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+};
+</script>
+<style scoped>
+.blub {
+  height: 100px;
+  background: #999;
+}
+</style>
